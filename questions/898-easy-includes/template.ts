0c6a7a8d37e2e5f772e5896ccae06fc0b466fede
RThong <403216075@qq.com>
2022-03-21 14:46:52 +0800

    feat: Includes Equal工具 数组递归

--- questions/898-easy-includes/template.ts
+++ questions/898-easy-includes/template.ts
@@ -1,1 +1,11 @@
-type Includes<T extends readonly any[], U> = any;
+type IsEqual<X, Y> = (<G>() => G extends X ? 1 : 2) extends <G>() => G extends Y
+  ? 1
+  : 2
+  ? true
+  : false
+
+type Includes<T extends readonly unknown[], U> = T extends [infer R, ...infer P]
+  ? IsEqual<R, U> extends true
+    ? true
+    : Includes<P, U>
+  : false
