9681180ecb7ab78a157091446ce7442ba28e0412
RThong <403216075@qq.com>
2022-03-21 15:55:39 +0800

    dev:html: Parameters unknown vs any

--- questions/3312-easy-parameters/template.ts
+++ questions/3312-easy-parameters/template.ts
@@ -1,1 +1,11 @@
-type MyParameters<T extends (...args: any[]) => any> = any
+// type MyParameters<T extends (...args: unknown[]) => unknown> = T extends (
+//   ...args: infer P
+// ) => unknown
+//   ? P
+//   : never
+
+type MyParameters<T extends (...args: any[]) => unknown> = T extends (
+  ...args: infer P
+) => unknown
+  ? P
+  : never
