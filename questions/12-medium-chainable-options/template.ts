17d36863ca520cda13cfd455af6c0314616013d1
RThong <403216075@qq.com>
2022-03-21 18:40:50 +0800

    feat: Chainable 链式调用 不断增强返回的类型

--- questions/12-medium-chainable-options/template.ts
+++ questions/12-medium-chainable-options/template.ts
@@ -1,4 +1,7 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+type Chainable<Res = {}> = {
+  option<T extends string, U>(
+    key: MyExclude<T, keyof Res>,
+    value: U
+  ): Chainable<Res & { [P in T]: U }>
+  get(): Res
 }
