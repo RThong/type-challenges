5de77e7e3335d40147127f17897fe19ae2ba17e2
RThong <403216075@qq.com>
2022-03-21 18:51:57 +0800

    dev:html: Chainable链式调用第二种方式

--- questions/12-medium-chainable-options/template.ts
+++ questions/12-medium-chainable-options/template.ts
@@ -5,3 +5,10 @@ type Chainable<Res = {}> = {
   ): Chainable<Res & { [P in T]: U }>
   get(): Res
 }
+
+// 使用interface this
+// type Not<T, K> = T extends K ? never : T;
+// interface Chainable {
+//   option<T extends string, U>(key: Not<T, keyof this>, value: U): this & { [K in T]: U };
+//   get(): Omit<this, 'option' | 'get'>
+// }
