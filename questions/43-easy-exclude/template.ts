73b75bf58ec4b9fe4f8aaa2f134816e3f97279a1
RThong <403216075@qq.com>
2022-03-21 10:59:59 +0800

    dev:html: 注释 完成Exclude

--- questions/43-easy-exclude/template.ts
+++ questions/43-easy-exclude/template.ts
@@ -1,1 +1,3 @@
-type MyExclude<T, U> = any
+// 联合类型会自动分发条件
+// 例如 T extends U ? X : Y, T 可能是 A | B 的联合类型, 那实际情况就变成(A extends U ? X : Y) | (B extends U ? X : Y)
+type MyExclude<T, U> = T extends U ? never : T
