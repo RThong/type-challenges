9e5487baa064ee5c66c8f0b76e529f3e0adf72c7
RThong <403216075@qq.com>
2022-03-22 17:17:52 +0800

    feat: LookUp 重要: 联合类型泛型参数的应用

--- questions/62-medium-type-lookup/template.ts
+++ questions/62-medium-type-lookup/template.ts
@@ -1,1 +1,15 @@
-type LookUp<U, T> = any
+// 基于下面这些注释，当U是联合类型的时候能满足要求
+type LookUp<U, T extends string> = U extends { type: T } ? U : never
+
+// U = Cat | Dog
+// 因为是泛型参数传入的联合类型，所以等价于下面这句
+// (Cat extends {type: T} ? Cat : never) | (Dog extends {type: T} ? Dog : never)
+// never | Dog
+// Dog
+
+// 普通联合类型的extends不同
+// type DD = 'x' | 'y' extends 'x' ? string: number
+
+// type AA<T> = T  extends 'x' ? string: number
+
+// type P = AA<'x' | 'y'>
