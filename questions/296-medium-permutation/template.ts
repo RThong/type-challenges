044d13b6316e7207789e2aea793c399f62e65916
RThong <403216075@qq.com>
2022-03-23 15:56:00 +0800

    feat: 重要 Permutation 联合类型转排列矩阵 联合类型参数泛型的应用

--- questions/296-medium-permutation/template.ts
+++ questions/296-medium-permutation/template.ts
@@ -1,1 +1,6 @@
-type Permutation<T> = any
+// 利用'A'|'B'这样的联合类型参数泛型 extends 'A'|'B' = XXX    可以让左边联合类型的每个子项都去走右边的逻辑
+type Permutation<T, U = T> = [T] extends [never]
+  ? []
+  : T extends U
+  ? [T, ...Permutation<Exclude<U, T>>]
+  : []
