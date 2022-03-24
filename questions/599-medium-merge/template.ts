53fa6930c758478c22d6fa7329d22940a8b59ba9
RThong <403216075@qq.com>
2022-03-24 17:42:41 +0800

    feat: Merge 合并对象类型  相同key后一个覆盖前一个

--- questions/599-medium-merge/template.ts
+++ questions/599-medium-merge/template.ts
@@ -1,1 +1,10 @@
-type Merge<F, S> = any;
+type Merge<
+  F extends { [key: string]: unknown },
+  S extends { [key: string]: unknown }
+> = {
+  [key in keyof F | keyof S]: key extends keyof S
+    ? S[key]
+    : key extends keyof F
+    ? F[key]
+    : never
+}
