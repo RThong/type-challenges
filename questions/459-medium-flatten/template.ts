679280f8b7a6f54ecdefcda3d99fa30b1637b475
RThong <403216075@qq.com>
2022-03-24 16:28:12 +0800

    dev:html: Flatten 使用第二个参数保存递归的值

--- questions/459-medium-flatten/template.ts
+++ questions/459-medium-flatten/template.ts
@@ -1,1 +1,22 @@
-type Flatten = any
+// type MyShift<T extends unknown[]> = T extends []
+//   ? []
+//   : T extends [infer _, ...infer Rest]
+//   ? Rest
+//   : never
+
+// type Flatten<T extends unknown[]> = T extends []
+//   ? []
+//   : T extends [infer A, ...infer Rest]
+//   ? A extends unknown[]
+//     ? [...Flatten<A>, ...Flatten<Rest>]
+//     : [A, ...Flatten<Rest>]
+//   : [T[0], ...MyShift<T>]
+
+type Flatten<T extends unknown[], U extends unknown[] = []> = T extends [
+  infer A,
+  ...infer Rest
+]
+  ? A extends unknown[]
+    ? Flatten<Rest, [...U, ...Flatten<A>]>
+    : Flatten<Rest, [...U, A]>
+  : U
