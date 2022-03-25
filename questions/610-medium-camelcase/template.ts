0c7a8e938ef48d02621541f2a4215a29485af59f
RThong <403216075@qq.com>
2022-03-25 15:22:13 +0800

    feat: CamelCase驼峰

--- questions/610-medium-camelcase/template.ts
+++ questions/610-medium-camelcase/template.ts
@@ -1,1 +1,36 @@
-type CamelCase<S> = any;
+type CurMap = {
+  a: "A"
+  b: "B"
+  c: "C"
+  d: "D"
+  e: "E"
+  f: "F"
+  g: "G"
+  h: "H"
+  i: "I"
+  j: "J"
+  k: "K"
+  l: "L"
+  m: "M"
+  n: "N"
+  o: "O"
+  p: "P"
+  q: "Q"
+  r: "R"
+  s: "S"
+  t: "T"
+  u: "U"
+  v: "V"
+  w: "W"
+  x: "X"
+  y: "Y"
+  z: "Z"
+}
+
+// 逻辑： -小写字母 转大写
+type MyCamelCase<S extends string> =
+  S extends `${infer A}-${infer Target}${infer B}`
+    ? Target extends keyof CurMap
+      ? `${A}${CurMap[Target]}${MyCamelCase<B>}`
+      : `${A}-${MyCamelCase<`${Target}${B}`>}`
+    : S
