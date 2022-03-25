7892d8bea5aaae3b5e479d7b46cd400c85d3c509
RThong <403216075@qq.com>
2022-03-25 15:53:49 +0800

    feat: KebabCase 羊肉串Case

--- questions/612-medium-kebabcase/template.ts
+++ questions/612-medium-kebabcase/template.ts
@@ -1,1 +1,39 @@
-type KebabCase<S> = any;
+type KebabMap = {
+  A: "a"
+  B: "b"
+  C: "c"
+  D: "d"
+  E: "e"
+  F: "f"
+  G: "g"
+  H: "h"
+  I: "i"
+  J: "j"
+  K: "k"
+  L: "l"
+  M: "m"
+  N: "n"
+  O: "o"
+  P: "p"
+  Q: "q"
+  R: "r"
+  S: "s"
+  T: "t"
+  U: "u"
+  V: "v"
+  W: "w"
+  X: "x"
+  Y: "y"
+  Z: "z"
+}
+
+type KebabCase<
+  S extends string,
+  U extends string = ""
+> = S extends `${infer Target}${infer R}`
+  ? Target extends keyof KebabMap
+    ? U extends ""
+      ? KebabCase<R, `${U}${KebabMap[Target]}`>
+      : KebabCase<R, `${U}-${KebabMap[Target]}`>
+    : KebabCase<R, `${U}${Target}`>
+  : U
