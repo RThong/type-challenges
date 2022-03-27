fa32c5fb9a3cdbc444114fc6d34c5461ccedc4fe
RThong <403216075@qq.com>
2022-03-27 11:34:42 +0800

    feat: Diff 两个对象类型不同的key

--- questions/645-medium-diff/template.ts
+++ questions/645-medium-diff/template.ts
@@ -1,1 +1,24 @@
-type Diff<O, O1> = any
+type Diff<
+  O extends { [key: string]: unknown },
+  O1 extends { [key: string]: unknown }
+> = {
+  [key in
+    | MyExclude<keyof O, keyof O1>
+    | MyExclude<keyof O1, keyof O>]: key extends keyof O1
+    ? O1[key]
+    : key extends keyof O
+    ? O[key]
+    : never
+}
+
+type Foo = {
+  name: string
+  age: string
+}
+type Bar = {
+  name: string
+  age: string
+  gender: number
+}
+
+type AA = Diff<Foo, Bar>
