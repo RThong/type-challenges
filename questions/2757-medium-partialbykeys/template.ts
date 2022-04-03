30eeedead9103cde0920e79efa772f1e1ee09da0
RThong <403216075@qq.com>
2022-04-03 12:46:40 +0800

    feat: 重要  PartialByKeys 标记某些属性为可选

--- questions/2757-medium-partialbykeys/template.ts
+++ questions/2757-medium-partialbykeys/template.ts
@@ -1,1 +1,18 @@
-type PartialByKeys<T , K> = any
+type MapObj<Obj extends Record<string, any>> = {
+  [Key in keyof Obj]: Obj[Key]
+}
+
+// {a:1,b:2}  和  {a:1} & {b:2}不一样
+type PartialByKeys<T extends Record<string, any>, K extends string = never> = [
+  K
+] extends [never]
+  ? Partial<T>
+  : MapObj<Omit<T, K> & { [key in Extract<keyof T, K>]?: T[key] }>
+
+// 更好的写法
+// type Merge<T, U> = Pick<T & U, keyof (T & U)>
+
+// type PartialByKeys<T, K = keyof T> = Merge<
+//   Pick<Partial<T>, Extract<keyof T, K>>,
+//   Pick<T, Exclude<keyof T, K>>
+// >
