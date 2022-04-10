547290d33efb38f0d834d1bd24765f40589cd7b9
RThong <403216075@qq.com>
2022-04-10 12:45:09 +0800

    dev:html: RequiredByKeys 指定key设置为Required  keyof any 能得到所有的对象类型key组成的联合类型

--- questions/2759-medium-requiredbykeys/template.ts
+++ questions/2759-medium-requiredbykeys/template.ts
@@ -1,1 +1,7 @@
-type RequiredByKeys<T , K> = any
+type Copy<T> = Pick<T, keyof T>
+
+// keyof any 能得到所有的对象类型key组成的联合类型
+type RequiredByKeys<
+  T extends Record<string, any>,
+  K extends keyof any = keyof T
+> = Copy<Omit<T, K> & Required<Pick<T, Extract<keyof T, K>>>>
