2d664e923074fd207ae7dc53dc317fbb1c7eb0f7
RThong <403216075@qq.com>
2022-04-23 11:28:12 +0800

    feat: 数字相加

--- questions/4182-medium-fibonacci-sequence/template.ts
+++ questions/4182-medium-fibonacci-sequence/template.ts
@@ -1,1 +1,26 @@
-type Fibonacci<T extends number> = any
+type ArrByNum<T extends number, U extends any[] = []> = U["length"] extends T
+  ? U
+  : ArrByNum<T, [...U, any]>
+
+type Plus<T extends number, U extends number> = [
+  ...ArrByNum<T>,
+  ...ArrByNum<U>
+]["length"]
+
+type LastTwoSum<T extends number[]> = T extends [...infer _, infer A, infer B]
+  ? A extends number
+    ? B extends number
+      ? Plus<A, B>
+      : never
+    : never
+  : never
+
+type Fibonacci<
+  T extends number,
+  U extends number[] = [1, 1]
+> = T extends U["length"]
+  ? Last<U>
+  : Fibonacci<
+      T,
+      [...U, ...(LastTwoSum<U> extends number ? [LastTwoSum<U>] : [])]
+    >
