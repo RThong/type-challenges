880e2516e5cb0c1f091ab9f39533d0eda54226ce
RThong <403216075@qq.com>
2022-04-27 11:41:34 +0800

    feat: without 数组中去除元素

--- questions/05117-medium-without/template.ts
+++ questions/05117-medium-without/template.ts
@@ -1,1 +1,17 @@
-type Without<T, U> = any
+type Without<
+  T extends unknown[],
+  U extends unknown[] | unknown,
+  K extends T[number][] = []
+> = T extends [infer First, ...infer Res]
+  ? Without<
+      Res,
+      U,
+      [
+        ...K,
+        // 数组转联合类型去判断
+        ...(First extends (U extends unknown[] ? U : [U])[number]
+          ? []
+          : [First])
+      ]
+    >
+  : K
