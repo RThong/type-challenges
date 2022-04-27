ecfc6d120e97f84fa42960c0e0f26c09d726492a
RThong <403216075@qq.com>
2022-04-27 17:49:35 +0800

    feat: 使用Equal去调整indexOf

--- questions/05153-medium-indexof/template.ts
+++ questions/05153-medium-indexof/template.ts
@@ -1,8 +1,11 @@
-type IndexOf<T extends unknown[], U, K extends T[number][] = []> = T extends [
-  infer L,
-  ...infer Res
-]
-  ? L extends U
+import { Equal } from "@type-challenges/utils"
+
+export type IndexOf<
+  T extends unknown[],
+  U,
+  K extends T[number][] = []
+> = T extends [infer L, ...infer Res]
+  ? Equal<L, U> extends true
     ? K["length"]
     : IndexOf<Res, U, [...K, L]>
   : -1
