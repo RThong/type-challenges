c71211c4ac3dbaaae74ea823aa78e892971284e4
RThong <403216075@qq.com>
2022-04-27 11:53:48 +0800

    feat: IndexOf

--- questions/05153-medium-indexof/template.ts
+++ questions/05153-medium-indexof/template.ts
@@ -1,1 +1,8 @@
-type IndexOf<T, U> = any
+type IndexOf<T extends unknown[], U, K extends T[number][] = []> = T extends [
+  infer L,
+  ...infer Res
+]
+  ? L extends U
+    ? K["length"]
+    : IndexOf<Res, U, [...K, L]>
+  : -1
