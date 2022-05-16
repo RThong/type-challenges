91db97ba82ed961ef2a0b7054e4ff09291bb7c7d
RThong <403216075@qq.com>
2022-05-16 23:01:28 +0800

    feat: NumberRange 数字范围联合类型

--- questions/08640-medium-number-range/template.ts
+++ questions/08640-medium-number-range/template.ts
@@ -1,1 +1,25 @@
-type NumberRange<L, H> = any
+type ArrLength<T extends unknown[]> = T["length"] extends number
+  ? T["length"]
+  : never
+
+type NumberRange<
+  L extends number,
+  H extends number,
+  A extends unknown[] = [],
+  B extends number[] = []
+> = A["length"] extends L
+  ? L extends H
+    ? [...B, A["length"]][number]
+    : NumberRange<
+        ArrLength<[...A, unknown]>,
+        H,
+        [...A, unknown],
+        [...B, A["length"]]
+      >
+  : NumberRange<L, H, [...A, unknown], B>
+// type Utils<L, C extends any[] = [], R = L> =
+//   C['length'] extends L
+//       ? R
+//       : Utils<L, [...C, 0], C['length'] | R>
+
+// type NumberRange<L, H> = L | Exclude<Utils<H>, Utils<L>>
