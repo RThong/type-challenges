808238c335ba66749ddae925da1cdc4e6f7e6a92
RThong <403216075@qq.com>
2023-01-31 14:27:53 +0800

    CamelCase

--- questions/00114-hard-camelcase/template.ts
+++ questions/00114-hard-camelcase/template.ts
@@ -1,1 +1,12 @@
-type CamelCase<S extends string> = any
+// type SplitWithChar<T extends string, CurrentStr extends string = '', Arr extends string[] = []> =
+// T extends `${infer L}${infer R}` ? L extends '_' ? SplitWithChar<R, '', [...Arr, CurrentStr]> : SplitWithChar<R, `${CurrentStr}${L}`, Arr> : [...Arr, CurrentStr]
+
+// type UppercaseFirst<T extends string> = T extends `${infer L}${infer R}` ? `${Uppercase<L>}${Lowercase<R>}` : never
+
+// type FormatArr<T extends string[], U extends string[] = []> = T extends [infer L, ...infer R] ? U['length'] extends 0 ?
+//   FormatArr<R, [Lowercase<L>]> : FormatArr<R, [...U, UppercaseFirst<L>]> : U
+
+// type CamelCase<T extends string> = T extends `${infer _}_${infer __}` ? Join<FormatArr<SplitWithChar<T>>, ''> : Lowercase<T>
+
+type CamelCase<S extends string> = S extends `${infer F}_${infer M}${infer E}` ?
+`${Lowercase<F>}${Uppercase<M>}${CamelCase<E>}` : Lowercase<S>
