01368a422601a5c60919817141e22a72838df12d
RThong <403216075@qq.com>
2023-02-16 17:06:27 +0800

    feat: Integer

--- questions/10969-medium-integer/template.ts
+++ questions/10969-medium-integer/template.ts
@@ -1,1 +1,14 @@
-type Integer<T> = any
+type AllZero<T extends string> = T extends `${infer L}${infer R}` ?
+  L extends '0' ? AllZero<R> : false : never
+
+type Integer<T extends number> = number extends T ? never : (`${T}` extends `${infer L}.${infer R}`
+  ? (AllZero<R> extends true ? L : never): T)
+
+// '1.1' === `${1.1}`
+// '1.0' !== `${1.0}`
+// 数字的1.0就等于1会直接被转成1
+
+// type Integer<T extends number> =
+//   number extends T?
+//     never:
+//       `${T}` extends `${string}.${string}`?never:T
