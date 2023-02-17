3b50336cf21312be1e472423b76f627d7093a352
RThong <403216075@qq.com>
2023-02-17 10:48:46 +0800

    feat: Filter

--- questions/18220-medium-filter/template.ts
+++ questions/18220-medium-filter/template.ts
@@ -1,1 +1,3 @@
-type Filter<T extends any[], P> = []
+type Filter<T extends any[], P, U extends any[] = []> = T extends [infer L, ...infer R]?
+
+  L extends P ? Filter<R, P, [...U, L]> : Filter<R, P, U> : U
